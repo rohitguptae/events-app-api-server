14305b339d01713ca9f82f00c81a3d2d938aebd0
RGCloudBootCamp <rohitguptae@gmail.com>
2022-04-28 16:07:47 +0000

    My first change

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Cloud BootCamp Team';
+    team = 'Cloud BootCamp Team - Fist Commit';
     version = '1.0.0';
 }
 module.exports = AppConfig;
